6fcfde43e2da7b6f76bf0970f854656d2966d397
Akshitha483 <akshithamn522@gmail.com>
2021-10-06 10:55:55 +0530

    adding src folder

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header>
+      <b>SHOPPING CART</b>
+      <button v-on:click="navigateTo('products')">View Products</button>
+      {{ cart.length }} in cart
+      <button v-on:click="navigateTo('cart')">View Cart</button>
+    </header>
+
+    <div v-if="page === 'cart'">
+      <Cart v-on:removeItemFromCart="removeItemFromCart" :cart="cart" />
+    </div>
+
+    <div v-if="page === 'products'">
+      <Products v-on:addItemToCart="addItemToCart" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Products from "./components/Products.vue";
+import Cart from "./components/Cart.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => {
+    return {
+      page: "products",
+      cart: [],
+      
+    };
+  },
+  methods: {
+    addItemToCart(product) {
+      this.cart.push(product);
+    },
+    removeItemFromCart(product) {
+      this.cart.splice(this.cart.indexOf(product), 1);
+    },
+    navigateTo(page) {
+      this.page = page;
+    },
+  },
+  components: { Products, Cart }
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+}
+
+.products {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.products button {
+  padding: 10px;
+  background-color: blue;
+  color: white;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  
+}
+
+b {
+  color: white;
+  float: left;
+}
+</style>
+
+<style scoped>
+header {
+  height: 60px;
+  background-color: blueviolet;
+  box-shadow: 2px 2px 5px wheat;
+  text-align: right;
+  font-size: 30px;
+  padding-top: 20px;
+}
+
+header button {
+  padding: 10px;
+  border: none;
+  cursor: pointer;
+  color: white;
+  background-color: green;
 }
 </style>
---
+++ src/components/Cart.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <h1>Your Cart</h1>
+      <div class="products">
+        <div v-for="(product, index) in cart" :key="index">
+         <h3> {{ product.name }}</h3>
+          <img :src="product.image" />
+          <div>{{ product.cost }}</div>
+          <button v-on:click="removeItemFromCart(product)">
+            Remove from cart
+          </button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["cart"],
+    methods: {
+        removeItemFromCart(index) {
+         this.$emit("removeItemFromCart", index);  
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
